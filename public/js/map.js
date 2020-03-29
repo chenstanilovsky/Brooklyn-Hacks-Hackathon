83969e5b92b215893663eeca50976f1691609739
Henry Stanilovsky <chen.stanilovsky92@myhunter.cuny.edu>
2020-03-29 15:42:49 -0400

    Added markers to google map with locations of homeless drop in centers

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,53 @@
+function generateMarkers(map){
+  $.getJSON('homeless_drop_in_center.json', function(json){
+    console.log('JSON Loaded');
+    let lat_idx = 13;
+    let lng_idx = 14;
+    for(let i = 0; i < json['data'].length; i++){
+      let lat = parseInt(json['data'][0][lat_idx]);
+      let lng = parseInt(json['data'][0][lng_idx]);
+      let marker = new google.maps.Marker({
+        position: {lat, lng},
+        map: map
+      });
+    }
+  });
+}
+function generateMap(center){
+}
 //require('dotenv').config();
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  let nyc_lat = 40.7128;
+  let nyc_lng = -74.006;
+  var nyc_lat_lng = {lat: nyc_lat, lng: nyc_lng};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-      });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
+    document.getElementById('map'), {
+      zoom: 10,
+      center: nyc_lat_lng
   });
+  $.getJSON('homeless_drop_in_center.json', function(json){
+    console.log('JSON Loaded');
+    console.log(json);
+    let lat_idx = 13;
+    let lng_idx = 14;
+    for(let i = 0; i < json['data'].length; i++){
+      let lat = parseFloat(json['data'][i][lat_idx]);
+      let lng = parseFloat(json['data'][i][lng_idx]);
+      console.log(lat + " " + lng);
+      let marker = new google.maps.Marker({
+        position: {lat, lng}
+      });
+      marker.setMap(map);
+    }
+  });
+
+  // The marker, positioned at Uluru
+  /*var marker = new google.maps.Marker({
+    position: nyc_lat_lng,
+    map: map
+  });*/
 }
 let apiScript = document.getElementById("apiScript");
 apiScript.src = "https://maps.googleapis.com/maps/api/js?key=**API HERE**&callback=initMap";
-//  <script async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap"></script>
