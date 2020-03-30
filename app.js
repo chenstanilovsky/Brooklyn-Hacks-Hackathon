08ad436dbe340031cc31529ee24893cd6c474b66
Resizt <resiztdzn@gmail.com>
2020-03-30 07:33:03 -0400

    Final version
    Map added
    Buttons added
    Landing page fix
    All pages done
    Fixed Links

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ let path = require('path');
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', function(req,res){
-  res.sendFile('/index.html');
+  res.sendFile('/pages/map.html');
 });
-app.get('/index.html', function(req,res){
-  res.sendFile('/index.html');
+app.get('/pages/map.html', function(req,res){
+  res.sendFile('/pages/map.html');
 });
 
-app.listen(process.env.PORT || 8000);
+app.listen(8000);
 /*const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
