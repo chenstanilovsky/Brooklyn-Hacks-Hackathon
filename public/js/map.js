7f451e22af378f37889ce175c06a925c5370e84f
Henry Stanilovsky <chen.stanilovsky92@myhunter.cuny.edu>
2020-03-29 22:14:37 -0400

    Update

--- public/js/map.js
+++ public/js/map.js
@@ -165,4 +165,4 @@ function generateMap(json_path, idxs, center){
 }
 
 let apiScript = document.getElementById("apiScript");
-apiScript.src = "https://maps.googleapis.com/maps/api/js?key=&callback=initMap";
+apiScript.src = "https://maps.googleapis.com/maps/api/js?key=<API KEY HERE>&callback=initMap";
